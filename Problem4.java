54249f82d82782a9452091cebb9bed2c4c93adac
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-08 10:35:25 +0530

    Problem4.java

---
+++ Problem4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Problem4{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int rad = sc.nextInt();
+	int area = (22/7) + rad*rad;
+	System.out.println("Area is:"+area);
+	}
+}
