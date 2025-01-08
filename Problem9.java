761fb41ec98ec40c3f611b0772c063e1fecb32ab
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-08 11:02:41 +0530

    Ninth Problem

---
+++ Problem9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem9{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int num1 = sc.nextInt();
+	int num2 = sc.nextInt();
+	int num3 = sc.nextInt();
+	int avg = (num1+num2+num3)/3;
+	System.out.println("Average is:"+avg);
+	}
+}
