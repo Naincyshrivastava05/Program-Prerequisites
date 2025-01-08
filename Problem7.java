fbbb633eb797240a2b593666f29e17db346ae9c1
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-08 10:52:36 +0530

    Seventh Problem

---
+++ Problem7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Problem7{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int length = sc.nextInt();
+	int width = sc.nextInt();
+	int para = 2*(length*width);
+	System.out.println("Perimeter is:"+para);
+	}
+}
