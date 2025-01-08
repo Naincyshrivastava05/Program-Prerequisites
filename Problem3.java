0e3c86c9bd1b6129188c96bac9461bfc7130ad33
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-08 10:16:55 +0530

    Third Problem

---
+++ Problem3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Problem3{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int temp = sc.nextInt();
+	int farenhite = (temp*9/5) + 32;
+	System.out.println("Temprature in Farenhite is:"+farenhite);
+	}
+}
